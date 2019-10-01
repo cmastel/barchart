e609a08c71da87705574f89e780702b8cfe99081
Chris Mastel <chris.mastel@gmail.com>
2019-10-01 19:45:05 +0000

    loop now creates the div and class for each bar

--- main.js
+++ main.js
@@ -7,31 +7,20 @@ function drawBarChart(data, options, element) {
     $(element).css('height', options['height']);
     $(element).css('background-color', options['bar-color']);
     $(element).css('grid-template-rows', data.length + ', 1fr');
-/*
-    $('.bar-1').css('grid-column-end', data[0]);
-    $('.bar-2').css('grid-column-end', data[1]);
-    $('.bar-3').css('grid-column-end', data[2]);
-    $('.bar-4').css('grid-column-end', data[3]);
-    $('.bar-5').css('grid-column-end', data[4]);
-    $('.bar-6').css('grid-column-end', data[5]);
-    $('.bar-7').css('grid-column-end', data[6]);
-*/
 
 
-    // Add value to each bar
-    for (let i = 0; i < 7; i++) {
+    // Create, and add Value to each bar
+    for (let i = 0; i < data.length; i++) {
       let bar = '.bar-' + (i + 1);
+      $('<div class="bar-' + (i + 1) +'">').appendTo(element);
       $(bar).css('grid-column-end', data[i]);
       $('<h1>' + data[i] + '</h1>').appendTo(bar);
-    } 
+    } // end of for loop
 
-    // This is not working - adding an additional Class (i.e. bar)
-    $('.bar-8)').addClass('chart');
-    $('.bar-8').css('grid-column-end', data[7]);
-  });
+  }); // end of $(document).ready
 
 
-}
+} // end of function drawBarChart
 
 let data = [50, 8, 80, 9, 100, 60, 40, 5];
 let data2 = [20, 50, 90];
