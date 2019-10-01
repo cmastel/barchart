fe66d9541f470a757c4f9a85a7303a74d462c411
Chris Mastel <chris.mastel@gmail.com>
2019-10-01 20:10:42 +0000

    incorporated chart header into grid, and allowed it to be changed

--- main.js
+++ main.js
@@ -1,20 +1,19 @@
 function drawBarChart(data, options, element) {
   $(document).ready(function(){
-    $("<h1>This is the Chart Title</h1>").prependTo(element);
-
   
     $(element).css('width', options['width']);
     $(element).css('height', options['height']);
     $(element).css('background-color', options['bar-color']);
     $(element).css('grid-template-rows', data.length + ', 1fr');
+    myHeader.innerText = options['chart-title'];
 
-
-    // Create, and add Value to each bar
+    // for each value in the data array, create a new div in the Chart
+    // element, assign it a length, and add the value into the bar
     for (let i = 0; i < data.length; i++) {
       let bar = '.bar-' + (i + 1);
       $('<div class="bar-' + (i + 1) +'">').appendTo(element);
       $(bar).css('grid-column-end', data[i]);
-      $('<h1>' + data[i] + '</h1>').appendTo(bar);
+      $('<h2>' + data[i] + '</h2>').appendTo(bar);
     } // end of for loop
 
   }); // end of $(document).ready
@@ -26,7 +25,8 @@ let data = [50, 8, 80, 9, 100, 60, 40, 5];
 let data2 = [20, 50, 90];
 let options = {'width': '70vw', 
                 'height': '50vh', 
-                'bar-color': '#cccccc'};
+                'bar-color': '#cccccc',
+                'chart-title': 'A Beautiful Bar Chart'};
 let element = '.chart'
 
 drawBarChart(data, options, element);
