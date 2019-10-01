c35a444891172cf7472561c09134cbad8cf6de3b
Chris Mastel <chris.mastel@gmail.com>
2019-10-01 17:30:12 +0000

    added the creation or bars into value loop

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function drawBarChart(data, options, element) {
     $(element).css('height', options['height']);
     $(element).css('background-color', options['bar-color']);
     $(element).css('grid-template-rows', data.length + ', 1fr');
-
+/*
     $('.bar-1').css('grid-column-end', data[0]);
     $('.bar-2').css('grid-column-end', data[1]);
     $('.bar-3').css('grid-column-end', data[2]);
@@ -15,13 +15,17 @@ function drawBarChart(data, options, element) {
     $('.bar-5').css('grid-column-end', data[4]);
     $('.bar-6').css('grid-column-end', data[5]);
     $('.bar-7').css('grid-column-end', data[6]);
+*/
+
 
     // Add value to each bar
     for (let i = 0; i < 7; i++) {
       let bar = '.bar-' + (i + 1);
+      $(bar).css('grid-column-end', data[i]);
       $('<h1>' + data[i] + '</h1>').appendTo(bar);
     } 
 
+    // This is not working - adding an additional Class (i.e. bar)
     $('.bar-8)').addClass('chart');
     $('.bar-8').css('grid-column-end', data[7]);
   });
