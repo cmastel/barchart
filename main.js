505a81f3b72b8c47aa03b078a5d3a94de5390415
Chris Mastel <chris.mastel@gmail.com>
2019-10-01 17:26:25 +0000

    added functionality to show value in each bar

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function drawBarChart(data, options, element) {
     $(element).css('height', options['height']);
     $(element).css('background-color', options['bar-color']);
     $(element).css('grid-template-rows', data.length + ', 1fr');
-  
+
     $('.bar-1').css('grid-column-end', data[0]);
     $('.bar-2').css('grid-column-end', data[1]);
     $('.bar-3').css('grid-column-end', data[2]);
@@ -15,14 +15,22 @@ function drawBarChart(data, options, element) {
     $('.bar-5').css('grid-column-end', data[4]);
     $('.bar-6').css('grid-column-end', data[5]);
     $('.bar-7').css('grid-column-end', data[6]);
-    
-  
+
+    // Add value to each bar
+    for (let i = 0; i < 7; i++) {
+      let bar = '.bar-' + (i + 1);
+      $('<h1>' + data[i] + '</h1>').appendTo(bar);
+    } 
+
+    $('.bar-8)').addClass('chart');
+    $('.bar-8').css('grid-column-end', data[7]);
   });
 
 
 }
 
-let data = [50, 8, 80, 9, 100, 60, 40];
+let data = [50, 8, 80, 9, 100, 60, 40, 5];
+let data2 = [20, 50, 90];
 let options = {'width': '70vw', 
                 'height': '50vh', 
                 'bar-color': '#cccccc'};
