9e95e64bf7bbc518805e981cc2cb05a13196ef5e
Chris Mastel <chris.mastel@gmail.com>
2019-10-02 20:15:41 +0000

    revised function to have an Object as an input instead of an Array

--- main.js
+++ main.js
@@ -9,25 +9,39 @@ function drawBarChart(data, options, element) {
 
     // for each value in the data array, create a new div in the Chart
     // element, assign it a length, and add the value into the bar
+    /*
     for (let i = 0; i < data.length; i++) {
       let bar = '.bar-' + (i + 1);
       $('<div class="bar-' + (i + 1) +'">').appendTo(element);
       $(bar).css('grid-column-end', data[i]);
       $('<h2>' + data[i] + '</h2>').appendTo(bar);
     } // end of for loop
+    */
+
+
+    let dataKeys = Object.keys(data);
+    // for each value in the data object, create a new div in the Chart
+    // element, assign it a length, and add the value into the bar
+    for (let i = 0; i < dataKeys.length; i++) {
+      let bar = '.bar-' + (i + 1);
+      let key = dataKeys[i];
+      $('<div class="bar-' + (i + 1) +'">').appendTo(element);
+      $(bar).css('grid-column-end', data[key]);
+      $('<h2>' + key + ': ' + data[key] + '</h2>').appendTo(bar);
+    } // end of for loop
+
 
   }); // end of $(document).ready
-
-
 } // end of function drawBarChart
 
 let data = [50, 8, 80, 9, 100, 60, 40, 5];
-let data2 = [20, 50, 90];
+let data2 = {"Mercury": 0, "Venus": 0, "Earth": 1, "Mars": 2, 
+              "Jupiter": 67, "Saturn": 62, "Uranus": 27, "Neptune": 14};
 let options = {'width': '70vw', 
                 'height': '50vh', 
                 'bar-color': '#cccccc',
                 'chart-title': 'A Beautiful Bar Chart'};
 let element = '.chart'
 
-drawBarChart(data, options, element);
+drawBarChart(data2, options, element);
 
